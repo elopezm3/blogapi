0e96811755e98809d2117250001a0de259ac9fe4
Esteban Lopez <estebanlopez_99@hotmail.com>
2023-03-10 16:44:14 -0500

    [ADD] create and update endpoint (using also rescue_from for tests)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,13 @@
 class PostsController < ApplicationController
+
+ rescue_from Exception do |exception|
+  render json: { error: exception.message }, status: :internal_error
+ end
+
+ rescue_from ActiveRecord::RecordInvalid do |exception|
+  render json: { error: exception.message }, status: :unprocessable_entity
+ end
+
   def index
     @posts = Post.where(published: true)
     render json: @posts, status: :ok
@@ -7,5 +16,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     render json: @post, status: :ok
+  end
+
+  def create
+    @post = Post.create!(create_params)
+    render json: @post, status: :created
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update!(update_params)
+    render json: @post, status: :ok
+  end
+
+  private
+
+  def create_params
+    params.require(:post).permit(:title, :content, :published, :user_id)
+  end
+
+  def update_params
+    params.require(:post).permit(:title, :content, :published)
   end
 end
--- test/factories/posts.rb
+++ test/factories/posts.rb
@@ -3,10 +3,7 @@ FactoryBot.define do
   factory :post do
     title { Faker::Lorem.sentence }
     content { Faker::Lorem.paragraph }
-    published {
-      r = rand(0..1)
-      r == 1
-    }
+    published { [true, false].sample }
     user
   end
 end
